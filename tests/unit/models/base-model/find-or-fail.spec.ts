9233ee3f34241107626aead7f00c2a4abf72b912
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-07-31 11:22:35 +0330

    Revert "temporarily remove unit tests"
    This reverts commit cfd1eb5c231c6d5bcea15b904ce692c22d865166.

---
+++ tests/unit/models/base-model/find-or-fail.spec.ts
@@ -0,0 +1,13 @@
+// import { findOrFail } from "../../../../src/models/Base.model";
+// import { groups } from "../../../../src/models/Group.model";
+// import { NotFound } from "../../../../src/modules/utilities/http-error";
+// import { tempGroup } from "../../../utility/temps";
+
+// describe("Find group by id", () => {
+//   it("Should throw 404 if group doesn't exist", () => {
+//     expect(() => findOrFail("someId", groups)).toThrow(NotFound);
+//   });
+//   it("Should return group", async () => {
+//     expect(findOrFail(tempGroup.id.value, groups)).toStrictEqual(tempGroup);
+//   });
+// });
